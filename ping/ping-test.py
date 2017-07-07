a0ad714fa579cd6177106bc9f1e4d3c5fcd863b6
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-07-06 22:29:19 -0500

    Added verbose mode for script ping-test.py

--- ping/ping-test.py
+++ ping/ping-test.py
@@ -15,7 +15,7 @@ import pingparsing
 import ping_parsers
 
 DEFAULT_PING_COUNT = 20
-
+VERBOSE = True
 
 def parse_option():
     parser = argparse.ArgumentParser()
@@ -27,6 +27,9 @@ def parse_option():
         "-o", "--outfile", required=False, help="Destination file for ping results")
     parser.add_argument(
         "-I", dest="interface", help="Network interface to use for pinging")
+    parser.add_argument(
+        "-s", "--silent", action="store_true", help="Don't print verbose output from the test")
+
 
     return parser.parse_args()
 
@@ -36,7 +39,12 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def main():
+    global VERBOSE
     options = parse_option()
+    if options.silent:
+        VERBOSE = False
+    verboseprint = print if VERBOSE else lambda *a, **k: None
+
     transmitter = pingparsing.PingTransmitter()
     transmitter.interface = options.interface
     #transmitter.waittime = 10
@@ -47,10 +55,18 @@ def main():
     hosts = os.path.join(scriptDir, options.pingfile)
     hostsFile = open(hosts, "r")
     lines = hostsFile.readlines()
-
+    if transmitter.interface is None:
+        verboseprint("Network Interface: Default")
+    else:
+        verboseprint("Network Interface: " + str(transmitter.interface))
+    verboseprint("\nPing Count: " + str(transmitter.count))
+    verboseprint("\nTotal Tests: " + str(file_len(options.pingfile)) + "\n")
+    n = 0
     for line in lines:
         if not line.startswith("#"):
+            verboseprint ("Test #" + str(n + 1) + ": ")
             line = line.strip()
+            verboseprint("Pinging Host " + line)
             transmitter.destination_host = line
             result = transmitter.ping()
             ping_parser = pingparsing.PingParsing()
@@ -58,10 +74,12 @@ def main():
                 ping_parser.parse(result)
                 host = [line,ping_parser]
                 ping_results.append(host)
+                verboseprint("PLC: {0} PLR: {1}% RTT_MIN: {2}ms RTT_MAX: {3}ms RTT_AVG: {4}ms\n".format(ping_parser.packet_loss_count,ping_parser.packet_loss_rate,ping_parser.rtt_min,ping_parser.rtt_max,ping_parser.rtt_avg))
             except AttributeError as e:
-                print ("Non-existent Host: " + line)
-                print ()
+                verboseprint("Non-existent Host: " + line + "\n")
                 #logger.debug(e)
+            #verboseprint("Results:")
+            n += 1
 
     csv_file = os.path.join(scriptDir, options.outfile)
     ping_parsers.csv_ping_parser(ping_results,csv_file)
