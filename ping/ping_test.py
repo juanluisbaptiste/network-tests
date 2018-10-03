538b07ae4b645dd27260dd4fa564c7bb33057db4
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-10-02 22:01:55 -0500

    More python linter fixes

--- ping/ping_test.py
+++ ping/ping_test.py
@@ -61,37 +61,37 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 def get_min_avg(results):
-    return get_avg(results,'rtt_min')
+    return get_avg(results, 'rtt_min')
 
 
 def get_max_avg(results):
-    return get_avg(results,'rtt_max')
+    return get_avg(results, 'rtt_max')
 
 
 def get_ping_avg(results):
-    return get_avg(results,'rtt_avg')
+    return get_avg(results, 'rtt_avg')
 
 
 def get_packetlostcount_avg(results):
-    return get_avg(results,'packet_loss_count')
+    return get_avg(results, 'packet_loss_count')
 
 
 def get_packetlostrate_avg(results):
-    return get_avg(results,'packet_loss_rate')
+    return get_avg(results, 'packet_loss_rate')
 
 
-def get_avg(results,metric):
+def get_avg(results, metric):
     val = 0
     for result in results:
-        val += getattr(result[1],metric)
-    return round(val/len(results),2)
+        val += getattr(result[1], metric)
+    return round(val/len(results), 2)
 
 
 def get_std_deviation(results):
     vals = []
     for result in results:
         vals.append(result[1].rtt_avg)
-    return round(numpy.std(vals)/len(results),2)
+    return round(numpy.std(vals)/len(results), 2)
 
 
 def main():
@@ -103,7 +103,7 @@ def main():
 
     transmitter = pingparsing.PingTransmitter()
     transmitter.interface = options.interface
-    #transmitter.waittime = 10
+    # transmitter.waittime = 10
     transmitter.count = options.count or DEFAULT_PING_COUNT
     ping_results = []
     scriptDir = os.getcwd()
@@ -121,7 +121,7 @@ def main():
     n = 0
     for line in lines:
         if not line.startswith("#"):
-            verboseprint ("Test #" + str(n + 1) + ": ")
+            verboseprint("Test #" + str(n + 1) + ": ")
             line = line.strip()
             verboseprint("Pinging Host " + line)
             transmitter.destination_host = line
@@ -129,7 +129,7 @@ def main():
             ping_parser = pingparsing.PingParsing()
             try:
                 ping_parser.parse(result)
-                ping_result = [line,ping_parser]
+                ping_result = [line, ping_parser]
                 ping_results.append(ping_result)
                 verboseprint("Min: {0} ms Max: {1} ms Average: {2} ms Packet Loss Count: {3} Packet Loss Rate: {4}%\n".format(ping_parser.rtt_min,ping_parser.rtt_max,ping_parser.rtt_avg,ping_parser.packet_loss_count,ping_parser.packet_loss_rate))
             except AttributeError as e:
@@ -137,14 +137,14 @@ def main():
             n += 1
     time_elapsed = (time.mktime(time.localtime()) - start)
     verboseprint("\nTime elapsed: " + str(time_elapsed) + " seconds")
-    #Calculate stats
+    # Calculate stats
     avg_min = get_min_avg(ping_results)
     avg_max = get_max_avg(ping_results)
     avg_ping = get_ping_avg(ping_results)
     avg_plc = get_packetlostcount_avg(ping_results)
     avg_plr = get_packetlostrate_avg(ping_results)
     std_deviation = get_std_deviation(ping_results)
-    overall = (options.count,time_elapsed,avg_min,avg_max,avg_ping,avg_plc,avg_plr,std_deviation, "v" + version)
+    overall = (options.count, time_elapsed, avg_min, avg_max, avg_ping, avg_plc, avg_plr, std_deviation, "v" + version)
     verboseprint("\nAverage min: " + str(avg_min) + " ms")
     verboseprint("Average max: " + str(avg_max) + " ms")
     verboseprint("Average ping: " + str(avg_ping) + " ms")
@@ -154,7 +154,7 @@ def main():
 
     if options.outfile:
         csv_file = os.path.join(scriptDir, options.outfile)
-        ping_parsers.csv_ping_parser(ping_results,csv_file,overall)
+        ping_parsers.csv_ping_parser(ping_results, csv_file,overall)
 
     return 0
 
