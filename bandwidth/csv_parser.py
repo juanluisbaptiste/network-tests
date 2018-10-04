ba7d078ab79a6e1c454775e62d487b6e8fe66a9b
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-10-03 23:44:29 -0500

    More python linter and readability fixes

--- bandwidth/csv_parser.py
+++ bandwidth/csv_parser.py
@@ -7,20 +7,27 @@
 
 import csv
 
+
 def csv_parser(results, csv_file, overall, filesize):
     overall_speeds = ()
     with open(csv_file, 'wb') as myfile:
         wr = csv.writer(myfile)
-        #TODO: Add date of test and test info to file
+        # TODO: Add date of test and test info to file
         overall_header = overall[0]
         overall_speeds_csv = overall[1]
         wr.writerow(overall_header)
         wr.writerow(overall_speeds_csv)
         wr.writerow([])
-        header = ["Sample#", "File Size", "Average Speed (MB/sec)", "Average Throughput (Mbps)"]
+        header = ["Sample#",
+                  "File Size",
+                  "Average Speed (MB/sec)",
+                  "Average Throughput (Mbps)"]
         wr.writerow(header)
         n = 1
         for result in results:
-            row = [n,filesize,round(result*0.000001,2),round(result*0.000008,2)]
+            row = [n,
+                   filesize,
+                   round(result*0.000001, 2),
+                   round(result*0.000008, 2)]
             wr.writerow(row)
             n += 1
