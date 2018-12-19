8daf649fedfe957ad99b187e20a29ea261798a70
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-12-18 20:45:20 -0500

    Fix some linting errors

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name="network-tests",
     version="0.1.4",
-    #packages=find_packages(),
-    packages=['ping','bandwidth'],
-    #scripts=['ping-test','download-speed','upload-speed'],
+    # packages=find_packages(),
+    packages=['ping', 'bandwidth'],
     scripts=['ping/ping_test.py', \
              'bandwidth/download_speed.py', \
              'bandwidth/upload_speed.py',\
@@ -12,25 +11,31 @@ setup(
              'upload-tester', \
              'ping-tester',\
              'common.py'],
+    # scripts=['ping-test','download-speed','upload-speed'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['requests','numpy','pingparsing','statistics'],
+    install_requires=['requests', \
+                      'numpy', \
+                      'pingparsing', \
+                      'statistics'],
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['hosts.txt'],
         # And include any *.msg files found in the 'hello' package, too:
-        #'hello': ['*.msg'],
+        # 'hello': ['*.msg'],
     },
 
     # metadata for upload to PyPI
     author="Juan Luis Baptiste",
     author_email="juan.baptiste@gmail.com",
-    description="Collection of scripts to do network tests like download/upload speeds, network latency (ping) and store results in a CSV file.",
+    description="Collection of scripts to do network tests like \
+                 download/upload speeds, network latency (ping) and store \
+                 results in a CSV file.",
     license="GPLv3",
     keywords="ping bandwidth download upload test",
-    url="https://github.com/juanluisbaptiste/network-tests",   # project home page, if any
+    url="https://github.com/juanluisbaptiste/network-tests",
 
     # could also include long_description, download_url, classifiers, etc.
 )
